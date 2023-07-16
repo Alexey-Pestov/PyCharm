d8751861d2b22a4edd594f4c749b04b5756934b2
ALEXEY <alexeypestov@gmail.com>
2023-07-16 19:43:11 +0300

    Advertisements

--- advertisements/app_adv/urls.py
+++ advertisements/app_adv/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import index, top_sellers
+from .views import index, top_sellers, adv_post, regist, log_in, pro_file
 
 urlpatterns = [
     path('', index, name='main-page'),
-    path('top-sellers/', top_sellers, name='top-sellers')
+    path('top-sellers/', top_sellers, name='top-sellers'),
+    path('advertisement-post/', adv_post, name='advertisement-post'),
+    path('register/', regist, name='register'),
+    path('login/', log_in, name='login'),
+    path('profile/', pro_file, name='profile')
 ]
--- advertisements/app_adv/views.py
+++ advertisements/app_adv/views.py
@@ -6,3 +6,11 @@ def index(request):
 
 def top_sellers(request):
     return render(request, 'top-sellers.html')
+def adv_post(request):
+    return render(request, 'advertisement-post.html')
+def regist(request):
+    return render(request, 'register.html')
+def log_in(request):
+    return render(request, 'login.html')
+def pro_file(request):
+    return render(request, 'profile.html')
